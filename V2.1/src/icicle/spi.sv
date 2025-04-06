8c6bcac648d74e9ab5375fe1121f5acbcbaf0a3d
Riley-Cox <rileyc39@gmail.com>
2025-04-05 17:30:39 -0700

    Added a counter to keep track of shifts

--- V2.1/src/icicle/spi.sv
+++ V2.1/src/icicle/spi.sv
@@ -39,6 +39,7 @@ module spi_controller #(
   logic                 spi_busy;
   logic                 spi_done;
   logic                 spi_done_ack;
+  logic [3:0]           bitCounter;
 
   logic [7:0] data_reg;
   logic [31:0] offset;
@@ -92,12 +93,16 @@ module spi_controller #(
     if (!reset_n)begin
       shift_reg <= '0;
     end
-    else if(start_latched)
+    else if(start_latched) begin
       shift_reg <= data_reg;
+      bitCounter <= 4'd8;
+    end
     else if(state == TRANSFER_HIGH) begin
       if (counter == 0) begin
-        if ((shift_reg << 1) != 0)
+        if (bitCounter > 1) begin
           shift_reg <= shift_reg << 1;
+          bitCounter <= bitCounter - 1;
+        end
         else begin
           shift_reg <= shift_reg;
         end
@@ -135,18 +140,18 @@ module spi_controller #(
         end
       end
       TRANSFER_LOW: begin
-        spi_clk = 1'b1;
+        spi_clk = 1'b0;
         spi_mosi = shift_reg[7];
         spi_done = 1'b0;
         spi_busy = 1'b1;
         spi_cs_n = 1'b0;
       end
       TRANSFER_HIGH: begin
-        spi_clk = 1'b0;
+        spi_clk = 1'b1;
         spi_done = 1'b0;
         spi_busy = 1'b1;
         spi_cs_n = 1'b0;
-        spi_mosi = '0;
+        spi_mosi = shift_reg[7];
       end
       FINISH: begin
           spi_cs_n = 1'b1;
@@ -185,7 +190,7 @@ module spi_controller #(
         end
         TRANSFER_HIGH: begin
           if (counter == 0) begin
-            if ((shift_reg << 1) != 0) begin
+            if (bitCounter > 1) begin
               nextState = TRANSFER_LOW;
             end else begin
               nextState     = FINISH;
