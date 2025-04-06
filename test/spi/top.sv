4a169374550c59351f677b48c716ec4d181248bb
Riley-Cox <rileyc39@gmail.com>
2025-04-05 22:37:51 -0700

    Better TB, able to have a zero in MSB

--- test/spi/top.sv
+++ test/spi/top.sv
@@ -94,13 +94,11 @@ module top;
 
     repeat (8) @(posedge spi_clk) begin
       mem_read(32'h00000008); // Read SPI_STATUS_ADDR
-        if (shift_cnt < 8) begin
         mosi = mosi | spi_mosi;
-        if (mosi[7] !== 1)begin
+        if (shift_cnt < 7)begin
         mosi = mosi << 1;
         shift_cnt++;
         end
-      end
     end
 
     
