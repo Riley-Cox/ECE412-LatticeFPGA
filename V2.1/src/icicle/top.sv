bf8e642afe9bca17b5fdb354c47eaae7099c439a
Mohalshaiji <111268081+Mohalshaiji@users.noreply.github.com>
2025-05-09 12:48:32 -0700

    Added correct single_cycle reset and fixed pll timing issues

--- V2.1/src/icicle/top.sv
+++ V2.1/src/icicle/top.sv
@@ -60,64 +60,17 @@ assign greset = reset | ssr;
    (* keep *) logic pll_clk;
     logic pll_locked_async;
 	
-	reg ff_reset;
-	reg reset_out;
-	logic icicle_reset;
-	always_ff @(posedge clk) begin
-		if(!ff_reset) begin
-			reset_out <= 1'b1;
-			ff_reset <= 1'b1;
-		end else begin
-			reset_out <= 1'b0;
-		end
-	end
-/**
-    pll pll (
-`ifdef ECP5
-        .clki(clk),
-        .clko(pll_clk),
-`else
-        .clock_in(clk),
-        .clock_out(pll_clk),
-`endif
-        .locked(pll_locked_async)
-    );
-**/
-logic unused_clk;
-/**
-pll u_pll (
-    .ref_clk_i(clk),     // 12 MHz input from HFOSC
-    .rst_n_i(1'b1),            // Active-low reset; hold high for normal operation
-    .lock_o(pll_locked_async),       // Indicates when PLL is stable
-    .outcore_o(unused_clk),       
-	.outglobal_o(pll_clk)
-);
-**/
-/**
+	
 
-    (* ORIG_MODULE_NAME="riscv_pll", LATTICE_IP_GENERATED="1" *) riscv_pll_ipgen_lscc_pll lscc_pll_inst (
-        .ref_clk_i(clk),
-        .rst_n_i(1'b1),
-        .feedback_i(1'b0),
-        .dynamic_delay_i(4'b0000),
-        .bypass_i(1'b0),
-        .latch_i(1'b0),
-        .lock_o(pll_locked_async),
-        .outcore_o(pll_clk),
-        .outglobal_o(),
-        .outcoreb_o(),
-        .outglobalb_o(),
-        .sclk_i(),
-        .sdi_i(),
-        .sdo_o()
-    );
-**/
-    pll_gen u_pll (
+	    pll_gen u_pll (
         .ref_clk_i(clk),
         .rst_n_i(1'b1),
         .outcore_o(pll_clk),
         .lock_o(pll_locked_async)
     );
+
+
+logic unused_clk;
 
 
     logic pll_locked;
@@ -145,7 +98,19 @@ assign leds = blink_counter[23:16];
             reset_count <= reset_count + pll_locked;
         end
     end	
-assign icicle_reset = reset | reset_out;
+	
+
+	
+logic greset, ssr, qBar /* syn_keep = 1 */; 
+logic q /* syn_keep = 1 */; 
+always_ff @(posedge pll_clk) begin
+	q <= r;
+end
+assign qBar = ~q;
+assign ssr = qBar & r;	
+	
+
+assign greset = reset | ssr;
 
     sync sync (
         .clk(pll_clk),
@@ -153,7 +118,7 @@ assign icicle_reset = reset | reset_out;
         .out(pll_locked)
     );
 
-    icicle icicle (
+      icicle icicle (
         .clk(pll_clk),
         .reset(greset),
 
