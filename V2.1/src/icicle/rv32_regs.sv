b406d3f364bfdc67002ebf1ea057fbd8d74ed025
Mohalshaiji <111268081+Mohalshaiji@users.noreply.github.com>
2025-05-15 18:08:56 -0700

    fix initializaition issue for initial address

--- V2.1/src/icicle/rv32_regs.sv
+++ V2.1/src/icicle/rv32_regs.sv
@@ -2,46 +2,41 @@
 `define RV32_REGS
 
 module rv32_regs (
-    input clk,
-    input stall_in,
-    input writeback_flush_in,
+    input  logic        clk,
+    input  logic        stall_in,
+    input  logic        writeback_flush_in,
 
     /* control in */
-    input [4:0] rs1_in,
-    input [4:0] rs2_in,
-    input [4:0] rd_in,
-    input rd_write_in,
+    input  logic [4:0]  rs1_in,
+    input  logic [4:0]  rs2_in,
+    input  logic [4:0]  rd_in,
+    input  logic        rd_write_in,
 
     /* data in */
-    input [31:0] rd_value_in,
+    input  logic [31:0] rd_value_in,
 
     /* data out */
     output logic [31:0] rs1_value_out,
     output logic [31:0] rs2_value_out
 );
-    logic [31:0] regs [31:0];
-    logic [4:0] rs1;
-    logic [4:0] rs2;
+    // 32-entry register file with initial zero content
+    logic [31:0] regs [31:0] = '{default:32'h0};
+    logic [4:0]  rs1;
+    logic [4:0]  rs2;
 
-    generate
-        genvar i;
-        for (i = 0; i < 32; i = i+1) begin
-            initial
-                regs[i] <= 0;
-        end
-    endgenerate
-
+    // Read ports (registered addresses)
     assign rs1_value_out = regs[rs1];
     assign rs2_value_out = regs[rs2];
 
+    // Address pipelining and write-back
     always_ff @(posedge clk) begin
         if (!stall_in) begin
             rs1 <= rs1_in;
             rs2 <= rs2_in;
         end
-
-        if (!writeback_flush_in && rd_write_in && |rd_in)
+        if (!writeback_flush_in && rd_write_in && |rd_in) begin
             regs[rd_in] <= rd_value_in;
+        end
     end
 endmodule
 
